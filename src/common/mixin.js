2425c41d50fa5f91568225f03b80ac67a1ceac3e
xzk <3489116257@qq.com>
2022-10-06 15:39:37 +0800

    2022/10/6

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -1,3 +1,4 @@
+import {POP, NEW, SELL} from "./const"
 import BackTop from "components/content/backTop/BackTop";
 export const backTopMixin = {
     components: {
@@ -18,3 +19,27 @@ export const backTopMixin = {
         },
     }
 }
+
+export const tabControlMixin = {
+    data: function () {
+      return {
+        currentType: POP
+      }
+    },
+    methods: {
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = POP
+            break
+          case 1:
+            this.currentType = NEW
+            break
+          case 2:
+            this.currentType = SELL
+            break
+        }
+        console.log(this.currentType);
+      }
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyLoad from 'vue-lazyload'
 
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  preLoad: 1,
+  loading: require('assets/img/common/placeholder.png')
+})
 
 //添加事件总线对象
 Vue.prototype.$bus = new Vue()
